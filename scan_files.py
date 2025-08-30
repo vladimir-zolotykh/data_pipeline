b4623b05cc8efd3cea15792414db00f39ca2419a
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-08-30 09:12:17 +0300

    Initial import of scan_files
    Has the yield_filenames() generator.

---
+++ scan_files.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+"""
+>>> list(yield_filenames("access-log-0108.*", "www"))
+['access-log-0108.bz2', 'access-log-0108.gz']
+>>>
+"""
+import os
+import fnmatch
+from typing import Generator
+import doctest
+
+
+def yield_filenames(pattern: str, top: str) -> Generator[str, None, None]:
+    for dirpath, dirnames, filenames in os.walk(top):
+        for fn in fnmatch.filter(filenames, pattern):
+            yield fn
+
+
+if __name__ == "__main__":
+    doctest.testmod()
