280a1c9533f55241fc3b94dd8932aae0106ff399
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-08-30 20:31:38 +0300

    Checkpoint: Incompatible types in "yield from"
    scan_files.py:51: error: Incompatible types in "yield from"

--- scan_files.py
+++ scan_files.py
@@ -6,17 +6,62 @@
 ['access-log-0108.bz2', 'access-log-0108.gz']
 >>>
 """
+import io
 import os
+import re
 import fnmatch
 from typing import Generator
-import doctest
+import gzip
+import bz2
+
+# import doctest
 
 
 def yield_lognames(pattern: str, top: str) -> Generator[str, None, None]:
+    """Log file filenames"""
+
     for dirpath, dirnames, filenames in os.walk(top):
         for fn in fnmatch.filter(filenames, pattern):
             yield fn
 
 
+def yield_logopen(
+    lognames: Generator[str, None, None],
+) -> Generator[io.BufferedIOBase, None, None]:
+    """Open log files"""
+
+    fo: gzip.GzipFile | bz2.BZ2File
+    for log in lognames:
+        if log.endswith(".gz"):
+            fo = gzip.open(log)
+        elif log.endswith(".bz2"):
+            fo = bz2.open(log)
+        else:
+            open(log)  # io.BufferedIOBase
+        yield fo
+        fo.close()
+
+
+def yield_lines(
+    logopens: Generator[io.BufferedIOBase, None, None],
+) -> Generator[str, None, None]:
+    """Lines from all chained log files"""
+
+    for gen in logopens:
+        yield from gen
+
+
+def filter_lines(
+    pattern: str, lines: Generator[str, None, None]
+) -> Generator[str, None, None]:
+    for line in lines:
+        if re.search(pattern, line):
+            yield line
+
+
 if __name__ == "__main__":
-    doctest.testmod()
+    # doctest.testmod()
+    lognames = yield_lognames("access-log*.*", "www")
+    openfiles = yield_logopen(lognames)
+    lines = yield_lines(openfiles)
+    filter_lines("(?i)python", lines)
