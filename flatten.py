f0b7a3e4686a5a9630a54f2ccccfa777b8bc1b0a
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-09-01 19:18:40 +0300

    Implement flatten(), add doctest

--- flatten.py
+++ flatten.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
+from collections.abc import Iterable
 
-nested_list = [
+
+nested_list_gemini = [
     [
         "first_level_string",
         "another_string",
@@ -15,12 +17,28 @@ nested_list = [
     ],
     [["a", "b", "c", "d"]],
 ]
+nested_list_gpt = [
+    [["apple", "banana", 1, 2], ["cherry", 3, 4, "date"]],
+    [["eggplant", 5, "fig"], ["grape", ["honeydew", 6, 7]]],
+]
 
 
 def flatten(items):
-    return items
+    """
+    >>> list(flatten(nested_list_gpt))
+    ['apple', 'banana', 1, 2, 'cherry', 3, 4, 'date', 'eggplant', 5, 'fig', 'grape', 'honeydew', 6, 7]
+    >>> list(flatten(nested_list_gemini))
+    ['first_level_string', 'another_string', 'one', 2, 'three', 4, 10, 20, 'apple', 50, 'banana', 60, 'a', 'b', 'c', 'd']
+    """
+
+    for item in items:
+        if isinstance(item, Iterable) and not type(item) in (str, bytes):
+            yield from flatten(item)
+        else:
+            yield item
 
 
 if __name__ == "__main__":
-    flat_list = flatten(nested_list)
-    print(flat_list)
+    import doctest
+
+    doctest.testmod()
